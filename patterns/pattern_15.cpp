c86fc5385489705e3e8783b6740c8840717b925f
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-05 11:48:49 +0530

    15

--- patterns/pattern_15.cpp
+++ patterns/pattern_15.cpp
@@ -1,13 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cin >> m;
+    int n = (m+1)/2;
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j<=i-1; j++){
+            cout<<"-";
+        }
 
         if (i % 2 != 0)
         {
-            for (int j = 1; j <= n; j++)
+            for (int j = 1; j <= m; j++)
             {
                 if (j % 2 != 0)
                     cout << 1;
                 else
                     cout << 0;
             }
+        }
+        for(int j = 1; j<=i-1; j++){
+            cout<<"-";
         }
         if (i % 2 == 0)
         {
@@ -20,3 +35,7 @@
             }
         }
     
+
+    cout << endl;
+    }
+}
