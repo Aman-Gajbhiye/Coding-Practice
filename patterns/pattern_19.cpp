311ca83c662a0f303380e809f86112ab6f854aa1
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-08 23:56:25 +0530

    c

---
+++ patterns/pattern_19.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cin>>m;
+    int n = (m+1)/2;
+    for(int i = 1; i <= n; i++ ){
+        for(int j = 1; j <= i-1; i++){
+            cout<<"_";
+        }
+        for(int j = 1; j <= m; j++ ){
+                if(j%2 == 0){
+                cout<<1;
+            }
+            else{
+                cout<<0;
+            }
+        }
+        cout<<endl;
+    }
+}
