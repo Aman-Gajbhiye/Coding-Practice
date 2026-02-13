abe3b20ff6febad15a1724bbb865842fd9317777
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-14 01:29:45 +0530

    u

--- assignmnet_3/7_nth_fibonacci.cpp
+++ assignmnet_3/7_nth_fibonacci.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int fibo(int n){
-    if(n == 1){
-        return 0;
+    if(n == 0 or n == 1){
+        return n;
     }
     else{
     return fibo(n-1)+fibo(n-2);
